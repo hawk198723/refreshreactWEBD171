9d30a1c31d4cecbfd8936db55c17b289fbdf1f36
hawk198723 <hawk198723@gmail.com>
2023-09-10 22:20:43 -0700

    week3

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import NavBar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>
 );
